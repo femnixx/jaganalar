1f872188775752657baec3b56e5cbf1082477e51
femnixx <femnitality@email.com>
2025-08-30 15:36:03 +0700

    Made some improts for sign in

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'Supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class Signin extends StatelessWidget {
-  const Signin({Key? key}) : super(key: key);
+class Signin extends StatefulWidget {
+  Signin({Key? key}) : super(key: key);
+
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
