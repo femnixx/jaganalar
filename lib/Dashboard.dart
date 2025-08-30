53038a6a6f756d62224a088d3e3fc46d8f3d7c16
femnixx <femnitality@gmail.com>
2025-08-31 05:48:45 +0700

    Added dashboard

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'Supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 class Dashboard extends StatelessWidget {
-  
-  
   const Dashboard({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
