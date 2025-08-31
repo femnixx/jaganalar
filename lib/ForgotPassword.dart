90d659e90cc7a0bd9b2d2570a1fb958683657c57
femnixx <femnitality@gmail.com>
2025-09-01 06:20:56 +0700

    Made it work slicing wise

--- lib/ForgotPassword.dart
+++ lib/ForgotPassword.dart
@@ -12,12 +12,13 @@ class _ForgotPasswordState extends State<ForgotPassword> {
 
   @override
   Widget build(BuildContext context) {
+    final bool isValid =
+        emailController.text.isNotEmpty && emailController.text != '';
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Title(
-          color: Colors.black, 
-          child: Text('Lupa Password')),
+        title: Title(color: Colors.black, child: Text('Lupa Password')),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -26,18 +27,17 @@ class _ForgotPasswordState extends State<ForgotPassword> {
             Padding(
               padding: const EdgeInsets.only(top: 32),
               child: TextField(
+                onChanged: (text) => {setState(() {})},
                 controller: emailController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Color(0xffDBDCDC))
+                    borderSide: BorderSide(color: Color(0xffDBDCDC)),
                   ),
                   hint: Text(
                     'Email',
-                    style: TextStyle(
-                      color: Color(0xff8B8F90)
-                    ),
-                  )
+                    style: TextStyle(color: Color(0xff8B8F90)),
+                  ),
                 ),
               ),
             ),
@@ -46,9 +46,33 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               'Masukkan email anda untuk proses verifikasi, kami akann nmengirimkan kode 5 digit ke email anda',
               style: TextStyle(
                 color: Color(0xff71717A),
-                fontWeight: FontWeight.normal
+                fontWeight: FontWeight.normal,
               ),
-            )
+            ),
+            SizedBox(height: 42),
+            ElevatedButton(
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(8),
+                  ),
+                ),
+                backgroundColor: WidgetStateProperty.all(
+                  isValid ? Color(0xff1C6EA4) : Color(0xffD7D7D7),
+                ),
+                shadowColor: WidgetStateProperty.all(Colors.transparent),
+                minimumSize: WidgetStateProperty.all(Size(double.infinity, 50)),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Kirim',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  color: isValid ? Colors.white : Colors.grey,
+                ),
+              ),
+            ),
           ],
         ),
       ),
